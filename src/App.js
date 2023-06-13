b7027a6e27ef40cd7a2c46dfb917f854fd065385
pmarcel-uw <pmarcel@uw.edu>
2023-06-12 22:27:01 -0700

    implemented PollForm.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ShopForm from './Forms/ShopForm';
 import EventForm from './Forms/EventForm';
 import PublicFigureForm from './Forms/PublicFigureForm';
 import PublicFigurePostForm from './Forms/PublicFigurePostForm';
+import PollForm from './Forms/PollForm';
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                     <Route path="/eventform" element={<EventForm />} />
                     <Route path="/publicfigureform" element={<PublicFigureForm />} />
                     <Route path="/publicfigurepostform" element={<PublicFigurePostForm />} />
+                    <Route path="/pollform" element={<PollForm />} />
                 </Routes>
             </Router>
         </div>
